cafddb7f04be349788bee92a20ec4c6da8649fb7
Sergei29 <34629784+Sergei29@users.noreply.github.com>
2018-04-11 18:43:46 +0300

    update with js features

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-$(document).ready(function(){
-	
-	/*By doing this, we hide the <ul> element that contains the sign buttons and show the sign in form.  */
-			$('#sign-btn').on('click', function(e){
+$(document).ready(function(){	
+	/* Intro Section: By doing this, we hide the <ul> element that contains the sign-in buttons and show the sign in form.  */
+		$('#sign-btn').on('click', function(e){
 		$(e.currentTarget).closest('ul').hide();
 		$('form#signin').fadeIn('fast');
 	});//end on
 
+/* SCROLL-TO-TOP PAGE ARROW: ----------------------------------------------------------
+		 firstly, shall define the current window scroll position with method .scrollTop() then,
+		 when the arrow shall appear visible - fadeIn/fadeOut after  scrolling down of 500px , with the delay of 300ms */
+		var scrolDown_px = 500;
+			var delayShow_ms = 300;
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > scrolDown_px) {
+					$('.back-to-top').fadeIn(delayShow_ms);
+				}else{
+					$('.back-to-top').fadeOut(delayShow_ms);
+				}
+			});//end scroll
+			/*second, shal make click-event returning back to the 
+		top of the window with jquery function .animate()*/
+		$('.back-to-top a').click(function(event){
+			event.preventDefault();
+			$('html, body').animate(
+				{scrollTop:0},
+				delayShow_ms
+				);
+			return false;
+		});//end click
 
-	}); //end ready
+		$('[data-toggle="tooltip"]').tooltip();
+/*-----------------------------------------------------------------------------------------*/
+}); //end ready
 
